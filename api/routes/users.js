02aaa130356f96fa09dd4e84ed8a3b813c384514
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-31 11:35:45 +0100

    refactor: Update user routes with CRUD operations
    The user routes have been updated to include CRUD operations for creating, retrieving, updating, and deleting users. This improves the functionality of the API and provides more flexibility for managing user data.

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,5 +5,11 @@ const UsersController = require("../controllers/users");
 const router = express.Router();
 
 router.post("/", UsersController.create);
+router.get("/", UsersController.getAll);
+router.get("/:id", UsersController.get);
+router.get("/", UsersController.getAll);
+router.put("/:id", UsersController.update);
+router.delete("/:id", UsersController.delete);
+
 
 module.exports = router;
