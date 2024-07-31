381e8caab3dc77ba584903e4d917cc69302fbc0a
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-31 12:49:51 +0100

    feat: Update signup function to include name parameter
    The `signup` function in `authentication.js` has been updated to include a `name` parameter. This change allows users to provide their name during the signup process.

--- frontend/src/services/userInfos.js
+++ frontend/src/services/userInfos.js
@@ -1,14 +1,14 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-const token = localStorage.getItem("token");
 
-export const getUserInfo = async () => {
+export const getUserInfo = async (token) => {
   const requestOptions = {
     method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
     },
   };
+  console.log("token, ", token);
   const response = await fetch(`${BACKEND_URL}/users`, requestOptions);
   if (response.status !== 200) {
     throw new Error("Unable to fetch user");
