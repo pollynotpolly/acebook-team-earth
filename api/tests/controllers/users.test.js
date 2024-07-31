8cdbce4de3b143a4d090c31b19817f99a2518f2b
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-31 11:35:52 +0100

    chore: Update user.test.js with additional user properties

--- api/tests/controllers/users.test.js
+++ api/tests/controllers/users.test.js
@@ -14,7 +14,7 @@ describe("/users", () => {
     test("the response code is 201", async () => {
       const response = await request(app)
         .post("/users")
-        .send({ email: "poppy@email.com", password: "1234" });
+        .send({ email: "poppy@email.com", password: "1234",name:"poppy" });
 
       expect(response.statusCode).toBe(201);
     });
@@ -22,7 +22,7 @@ describe("/users", () => {
     test("a user is created", async () => {
       await request(app)
         .post("/users")
-        .send({ email: "scarconstt@email.com", password: "1234" });
+        .send({ email: "scarconstt@email.com", password: "1234", name:"scarconstt" });
 
       const users = await User.find();
       const newUser = users[users.length - 1];
