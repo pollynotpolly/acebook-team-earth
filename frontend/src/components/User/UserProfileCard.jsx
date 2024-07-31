1dbbb0f467349756800d0e84e9e7a26923988e11
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-31 11:36:37 +0100

    feat: Implement user profile card component
    The `UserProfileCard` component is added to display the user's name, picture, and about me information at the top of their profile. Currently, the image functionality is not implemented.

---
+++ frontend/src/components/User/UserProfileCard.jsx
@@ -0,0 +1,17 @@
+// This component is the card that displays the user's name, picture and about me info that we'd expect at the top of the user's profile.
+// Currently images wll not be implemented.
+
+import React from 'react';
+
+
+export const UserProfileCard = ({ user }) => {
+    return (
+        <div className='user-profile-card'>
+            <div className='image placeholder'>This will be where the image is
+                {/* <img src={user.image} alt={user.name} /> */}
+            </div>
+        <h2>{user.name}</h2>
+        <p>{user.about}</p>
+        </div>
+    );
+    }
