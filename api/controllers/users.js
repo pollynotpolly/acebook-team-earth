0f539d43aff6381c5173caa6f9e79115d120c142
“Gee2563” <george.farhatsmith@gmail.com>
2024-07-31 11:35:33 +0100

    refactor: Update user controller with async/await syntax
    The user controller has been refactored to use async/await syntax for better readability and error handling. This improves the overall code structure and makes it easier to maintain.

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,24 +1,78 @@
 const User = require("../models/user");
 
-const create = (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+// todo: add token checks?
 
-  const user = new User({ email, password });
-  user
-    .save()
-    .then((user) => {
-      console.log("User created, id:", user._id.toString());
-      res.status(201).json({ message: "OK" });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).json({ message: "Something went wrong" });
-    });
+const create = async (req, res) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = new User({ email, password, name, about: "" });
+
+    await user.save();
+    console.log("User created, id:", user._id.toString());
+    res.status(201).json({ message: "OK" });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
 };
+
+const getAll = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({message: 'OK', users: users});
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+}
+
+
+const get = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({message: 'OK', user: user});
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user.about = req.body.about;
+    user.name = req.body.name;
+    await user.save();
+    console.log('User updated, id:', user._id.toString());
+    res.status(200).json({message: 'OK'});
+  } catch (error) {
+    console.error(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+}
+
+const remove = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    await user.delete();
+    console.log('User deleted, id:', user._id.toString());
+    res.status(200).json({message: 'OK'});
+  } catch (error) {
+    console.error(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+}
 
 const UsersController = {
   create: create,
+  get: get,
+  update: update,
+  delete: remove,
+  getAll : getAll
 };
 
 module.exports = UsersController;
