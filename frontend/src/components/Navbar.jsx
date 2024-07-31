b66ed4aaf838c80a5ba1d7858af88e0f864e72f1
Polly <randlepolly@gmail.com>
2024-07-31 14:00:29 +0100

    comments now in code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export const Navbar = () => {
-    const navigate = useNavigate();
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();   // useNavigate hook - used to accesses the React Router history object 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);   // // useState to keep track of user's login status
 
     useEffect(() => {
-    const token = localStorage.getItem("token");
-    setIsLoggedIn(!!token);
+    const token = localStorage.getItem("token");    // Check if there's a token in localStorage
+    setIsLoggedIn(!!token);   // // Set isLoggedIn to true if token exists, false otherwise
     }, []);
 
-    const handleLogout = () => {
-    localStorage.removeItem("token");
+    const handleLogout = () => {   // Function to handle user logout
+    localStorage.removeItem("token");   // Update state to reflect logged out status
     setIsLoggedIn(false);
-    navigate("/login");
+    navigate("/login");       // Redirect user to login page
     };
 
     return (
@@ -22,7 +22,10 @@ export const Navbar = () => {
         <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/posts">Feed</Link></li>
-        {isLoggedIn ? (
+
+{/* Conditional rendering belwo based on login status using ternary operator - same as if else statements */}
+
+        {isLoggedIn ? (            
             <li><button onClick={handleLogout}>Logout</button></li>
         ) : (
             <>
