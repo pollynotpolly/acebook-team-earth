e384017fd10baac32aa18e3f35f61adbb277361a
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-06 12:47:46 +0100

    feat: Add FriendCard component for displaying friend information

---
+++ frontend/src/components/Friend/FriendCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export const FriendCard = ({ friend }) => {
+    return (
+        <div className='friend-card'>
+            <div className='image'>
+                {friend.image ? (
+                    <img src={friend.image} alt={friend.name} />
+                ) : (
+                    <div className='placeholder'>No Image</div>
+                )}
+            </div>
+            <h2>{friend.name} {friend.surname}</h2>
+            <Link to={`/friend-profile/${friend._id}`}>View Profile</Link>
+        </div>
+    );
+};
