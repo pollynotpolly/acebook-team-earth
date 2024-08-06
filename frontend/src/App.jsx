4740ce4d96603094a53dfbceb086c5d318ac49d1
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-06 12:46:58 +0100

    feat: Add MyFriends page to user routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { FeedPage } from "./pages/Feed/FeedPage";
 import { UserProfilePage } from "./pages/UserProfile/UserProfilePage";
 import { Layout } from "./components/Layout";
 import {EditProfile} from "./components/User/EditProfile";
+import {MyFriends} from "./pages/MyFriends"
 
 // docs: https://reactrouter.com/en/main/start/overview
 const router = createBrowserRouter([
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
       {
         path: '/my-profile-settings',
         element: <EditProfile  />,
+      },
+      {
+        path: '/my-friends',
+        element: <MyFriends />,
       },
     ],
   },
