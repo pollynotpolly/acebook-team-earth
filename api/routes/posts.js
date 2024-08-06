243fe5418db04d9896a58182c4bfc9318218ca80
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-06 16:40:57 +0100

    chore: Refactor comment routes and controllers

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -6,22 +6,14 @@ const PostsController = require("../controllers/posts");
 router.get("/", PostsController.getAllPosts);
 router.post("/", PostsController.createPost);
 
+const commentController = require("../controllers/comments");
+
+
 //DELETE a post
 //router.delete("/", PostsController.deletePost);
 
 //UPDATE a post
 //router.patch("/", Postcontroller.updatePost);
-
-const commentController = require("../controllers/comments");
-
-// Get all comments
-router.route("/comments")
-    .get(commentController.getAllComments)
-    .post(commentController.createComment);
-router.route("/comments/:id")
-    .get(commentController.getComment)
-    .delete(commentController.deleteComment)
-    .put(commentController.updateComment);
 
 
 module.exports = router;
