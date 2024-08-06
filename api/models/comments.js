293506d78b50b6f0fe371b830eb71b0c79874842
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-06 16:40:46 +0100

    Refactor comment model to use user_id and post_id instead of user and post

--- api/models/comments.js
+++ api/models/comments.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const CommentSchema = new mongoose.Schema({
     content: {type: String, required: true}, 
     date: {type: Date, default: Date.now},
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    post_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
 });
 
 const Comment = mongoose.model("Comment", CommentSchema);
