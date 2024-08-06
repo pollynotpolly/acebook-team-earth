91f3cef32060764a2e4546a6dad4c9e2299b9e1c
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-06 12:46:03 +0100

    feat: Add friend-related functionality to user routes

--- api/routes/profiles.js
+++ api/routes/profiles.js
@@ -11,5 +11,15 @@ router.put("/", userController.updateUser);
 //Delete user by id
 router.delete("/", userController.deleteUser)
 
+//Add friend to user
+router.post("/friends", userController.addFriend);
+//Remove friend from user
+router.delete("/friends", userController.removeFriend);
+
+//Get friends
+router.get("/friends", userController.getFriends);
+//Get non friends
+router.get("/nonfriends", userController.getNonFriends);
+
 
 module.exports = router;
