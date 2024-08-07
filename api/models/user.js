d2e788b44092f99bec8c20754d18858c991f2e9d
Polly <randlepolly@gmail.com>
2024-08-07 09:47:44 +0100

    no duplicates

--- api/models/user.js
+++ api/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, //added unique: true to the email field - creates a unique index in the database, preventing two documents from having the same email address.
   password: { type: String, required: true },
   about: { type: String, required: false },
   name: { type: String, required: true },
