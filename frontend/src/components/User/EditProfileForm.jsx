783237ddd1f521fe70e15cba6959b6a2910cf3de
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-08 11:41:42 +0100

    chore: Rename EditProfile.jsx to EditProfileForm.jsx and update form fields

--- frontend/src/components/User/EditProfileForm.jsx
+++ frontend/src/components/User/EditProfileForm.jsx
@@ -1,22 +1,14 @@
 import React  from "react";
 import { updateUserInfo } from "../../services/userInfos";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-export const EditProfile =(props) => {
-    const [userName, setUsername] = useState("");
-    const [userSurname, setUserSurname] = useState("");
-    const [userAbout, setUserAbout] = useState("");
+export const EditProfileForm =(props) => {
+    const [userName, setUserName] = useState(props.name);
+    const [userSurname, setUserSurname] = useState(props.surname);
+    const [userAbout, setUserAbout] = useState(props.about);
     const navigate = useNavigate();
 
-    const handleName = (e) => {
-        setUsername(e.target.value);
-    }
-
-    const handleSurname = (e) => {
-        setUserSurname(e.target.value);
-    }
 
     const handleAbout = (e) => {
         setUserAbout(e.target.value);
@@ -42,37 +34,16 @@ export const EditProfile =(props) => {
     };
 
     return (
-        <div className="edit-profile-container">
-            <h2>edit profile</h2>
-        <div className="edit-profile">
+    <>
         <form onSubmit={handleSubmit}>
             <label>
                 Change Profile picture:
-            </label>
             <input
                     type="text"
                     name="name"
                     ></input>
-            <label >
-                Name:
-                <input
-                    type="text"
-                    name="name"
-                    required={true}
-                    value={userName}
-                    onChange={handleName}
-                />
-            </label>
-            <label>
-                Surname:
-                <input
-                    type="text"
-                    name="surname"
-                    required={true}
-                    value={userSurname}
-                    onChange={handleSurname}
-                />
-            </label>
+                    </label>
+
             <label>
                 About:
                 <textarea
@@ -86,7 +57,7 @@ export const EditProfile =(props) => {
             
             </div>
             </form>
-            </div>
-            </div>
+          
+           </>
     )
 }
