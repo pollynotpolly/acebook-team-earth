e98d687cebad5c330b4a1fd7d7bb8dcfc0c0a3c0
“Gee2563” <george.farhatsmith@gmail.com>
2024-08-08 11:40:58 +0100

    feat: Add AddFriendButton component for adding friends

---
+++ frontend/src/components/Friend/AddFriendButton.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { addFriend } from "../../services/userInfos";
+
+export const AddFriendButton = (props) => {
+    console.log('Profile ID:', props);
+
+
+
+    const handleAddFriend = async (e) => {
+        e.preventDefault();
+        try {
+            const token = localStorage.getItem("token");
+            await addFriend(token, props.profileId);
+            alert("Friend added successfully");
+        } catch (error) {
+            console.error("Failed to add friend:", error);
+            alert("Failed to add friend");
+        }
+    }
+
+
+    return (
+        <div className="add-friend-button">
+            <button onClick={handleAddFriend}>Add Friend</button>
+        </div>
+    );
+}
